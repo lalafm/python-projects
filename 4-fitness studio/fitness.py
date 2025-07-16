ec31c0140bfb6de9fe3461b30782ca9337e5fad2
Lais Martins <lais@macbookair.home>
2025-07-16 22:06:37 +0100

    4- Solve fourth (final) research question

--- 4-fitness studio/fitness.py
+++ 4-fitness studio/fitness.py
@@ -2,12 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Load and clean dataframes
+# Load dataframes
 workout = pd.read_csv("data/workout.csv")
 keywords = pd.read_csv("data/three_keywords.csv")
 geo = pd.read_csv("data/workout_geo.csv")
 words_geo = pd.read_csv("data/three_keywords_geo.csv")
-# Remove rows without valid data
+# Clean - remove rows without valid data
 geo.dropna(inplace=True)
 words_geo.dropna(inplace=True)
 
@@ -50,10 +50,13 @@ selected_countries = ['United States', 'Australia', 'Japan']
 geo = geo[geo['country'].isin(selected_countries)]
 # Find row
 top_country = geo[(geo['workout_2018_2023'] == geo['workout_2018_2023'].max())]['country'].tolist()[0]
-print('\nCountry with highest interest in workouts is', top_country)
+print('\nThe country with highest interest in workouts is', top_country)
 print(geo)
 
 # 4: Find, between Philippines and Malaysia which country has highest interest in home workouts
- 
+country_filt = words_geo[(words_geo['Country'] == 'Philippines') | (words_geo['Country'] == 'Malaysia')]
+max_value = country_filt['home_workout_2018_2023'].max()
+home_workout_geo = country_filt[country_filt['home_workout_2018_2023'] == max_value]['Country'].tolist()[0]
+print('\nThe country with highest interest in home workouts is', home_workout_geo)
 
 plt.show()
