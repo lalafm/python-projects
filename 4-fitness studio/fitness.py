2fa2d2a710a6425285861b2551950a798d1fda3b
Lais Martins <lais@macbookair.home>
2025-07-16 16:45:00 +0100

    4- Initial commit

---
+++ 4-fitness studio/fitness.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+workout = pd.read_csv("data/workout.csv")
+keywords = pd.read_csv("data/three_keywords.csv")
+geo = pd.read_csv("data/workout_geo.csv")
+
+# Information about dataframe
+
+
+# 1: Year of peak interest on "workout" 
+
+# 2: Find most popular keyword during covid and now
+
+
+# 3: Find country with highest interest for workouts among: United States, Australia and Japan
+
+# 4: Find, between Philippines and Malaysia which country has highest interest in home workouts
+ 
+plt.show()
