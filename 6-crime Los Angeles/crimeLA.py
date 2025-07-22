f26ac2cae20e765b36b8655e4db1d21cedbe8fa1
Lais Martins <lais@macbookair.home>
2025-07-22 11:07:46 +0100

    6- Solve first research question

--- 6-crime Los Angeles/crimeLA.py
+++ 6-crime Los Angeles/crimeLA.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load dataframes
-crime = pd.read_csv('data/crimes.csv')
-print(crime.head())
+crime = pd.read_csv('data/crimes.csv', dtype={"TIME OCC": str})
 
 # Question 1: find the hour with the highest frequency of crimes and store in int variable 'peak_crime_hour'
+crime['hour'] = crime['TIME OCC'].str[:2].astype(int)
+peak_crime_hour = crime.value_counts('hour').index[0]
+print(f'Peak crime hour is: {peak_crime_hour}h')
