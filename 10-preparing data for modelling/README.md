707225d80a83e0556340162d52fc5391129d6d16
Lais Martins <lais@macbookair.home>
2026-02-19 15:40:37 +0000

    10- Solve research questions

--- 10-preparing data for modelling/README.md
+++ 10-preparing data for modelling/README.md
@@ -13,7 +13,7 @@ The Head Data Scientist at Training Data Ltd. has asked you to create a DataFram
 3. Columns containing floats must be stored as 16-bit floats (float16).
 4. Columns containing nominal categorical data must be stored as the category data type.
 5. Columns containing ordinal categorical data must be stored as ordered categories, and not mapped to numerical values, with an order that reflects the natural order of the column.
-5. The DataFrame should be filtered to only contain students with 10 or more years of experience at companies with at least 1000 employees, as their recruiter base is suited to more experienced professionals at enterprise companies.
+6. The DataFrame should be filtered to only contain students with 10 or more years of experience at companies with at least 1000 employees, as their recruiter base is suited to more experienced professionals at enterprise companies.
 - If you call .info() or .memory_usage() methods on ds_jobs and ds_jobs_transformed after you've preprocessed it, you should notice a substantial decrease in memory usage.
 
 The data:
--- 10-preparing data for modelling/prepdata.py
+++ 10-preparing data for modelling/prepdata.py
@@ -1,3 +1,64 @@
 import pandas as pd
 
+customer = pd.read_csv("data/customer_train.csv")
+print(customer.info())
+print(customer.memory_usage())
+print(customer["training_hours"].value_counts())
 
+# First task: change gender, relevant_experience, job_change to boolean type
+#gender_replace_map = {"Female":True, "Male":False}
+rel_exp_replace_map = {"Has relevant experience":True, "No relevant experience":False}
+customer["job_change"] = customer["job_change"].astype("boolean")
+#customer["gender"].replace(gender_replace_map, inplace=True)
+customer["relevant_experience"] = customer["relevant_experience"].replace(rel_exp_replace_map).astype("boolean")
+
+# Second task: Columns containing only int should have type int32 (student_id, training_hours)
+customer["student_id"] = customer["student_id"].astype("int32")
+customer["training_hours"] = customer["training_hours"].astype("int32")
+
+# Third task: Columns containing floats must be stored as float16 (city_development_index)
+customer["city_development_index"] = customer["city_development_index"].astype("float16")
+
+# Fourth task: Columns containing nominal categorical data must be stored as the category data type 
+# (city, sex, major_discipline, company_type)
+category_cols = ["city", "gender", "education_level", "major_discipline", "company_type"]
+customer[category_cols] = customer[category_cols].astype("category")
+
+# Fifth task: Columns containing ordinal categorical data must be stored as ordered categories
+# (experience, enrolled_university, company_size, last_new_job)
+ordered_cols = [ "enrolled_university", "education_level", "experience", "company_size", "last_new_job"]
+customer[ordered_cols] = customer[ordered_cols].astype("category")
+
+enrolled_order = ["no_enrollment", "Part time course", "Full time course"]
+edu_level_order = ["Primary School", "High School", "Graduate", "Masters", "Phd"]
+exp_order = ['<1'] + [str(i) for i in range(1,21)] + ['>20']
+comp_size_order = ["<10", "10-49", "50-99", "100-499", "500-999", "1000-4999", "5000-9999", "10000+"]
+last_job_order = ["never"] + [str(i) for i in range(1,5)] + [">4"]
+
+customer["enrolled_university"] = customer["enrolled_university"].cat.reorder_categories(
+    new_categories = enrolled_order,
+    ordered = True
+)
+
+customer["education_level"] = customer["education_level"].cat.reorder_categories(
+    new_categories = edu_level_order,
+    ordered = True
+)
+
+customer["experience"] = customer["experience"].cat.reorder_categories(
+    new_categories = exp_order,
+    ordered = True
+)
+customer["company_size"] = customer["company_size"].cat.reorder_categories(
+    new_categories = comp_size_order,
+    ordered = True
+)
+customer["last_new_job"] = customer["last_new_job"].cat.reorder_categories(
+    new_categories =last_job_order,
+    ordered = True
+)
+
+# Sixth task: Filter df to only contain students with 10+ years of experience at companies with at least 1000 employees
+customer_filtered = customer[(customer["experience"] >= "10") & (customer["company_size"] >= "1000-4999")]
+
+print(customer.info())
